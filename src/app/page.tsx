47a8d733a282b5ea29d829143c468fd4bf7e50f3
786haider <haiderhussain536@gmail.com>
2024-12-06 12:23:28 -0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import TodoApp from "./todo/page";
 
 export default function Home() {
   return (
-    <div className="flex-col justify-center items-center ">
-      <div className="h-auto max-w-3xl mt-20 ml-72 mb-44">
-        <h1 className="text-2xl md:text-4xl font-extrabold flex justify-center mt-10">
+    <div className="md:flex-col md:justify-center md:items-center ">
+      <div className="h-auto max-w-3xl mt-20 md:ml-72 mb-44">
+        <h1 className="sm:text-xl lg:text-3xl xl:text-5xl md:text-4xl font-extrabold flex justify-center mt-10">
           <i> Wellcome to My Todo App</i>
         </h1>
         <p className="text-lg md:text-2xl flex justify-center mt-5">
